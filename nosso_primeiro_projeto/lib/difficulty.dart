b2d45c3a1e77a1d86cbae7fba7bb403379f6df55
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-09-19 07:43:51 -0300

    refactor: separando responsabilidade em arquivos

---
+++ nosso_primeiro_projeto/lib/difficulty.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/task.dart';
+
+class Difficulty extends StatelessWidget {
+
+  final int dificultyLevel;
+
+  const Difficulty({
+    required this.dificultyLevel,
+    super.key,
+    required this.widget,
+  });
+
+  final Task widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.star, size: 15, color: (widget.dificuldade >= 1) ? Colors.blue : Colors.blue[100],),
+        Icon(Icons.star, size: 15, color: (dificultyLevel >= 2) ? Colors.blue : Colors.blue[100],),
+        Icon(Icons.star, size: 15, color: (dificultyLevel >= 3) ? Colors.blue : Colors.blue[100]),
+        Icon(Icons.star, size: 15, color: (dificultyLevel >= 4) ? Colors.blue : Colors.blue[100]),
+        Icon(Icons.star, size: 15, color: (dificultyLevel >= 5) ? Colors.blue : Colors.blue[100]),
+      ],
+    );
+  }
+}
