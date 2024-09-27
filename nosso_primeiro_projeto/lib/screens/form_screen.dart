34f13666784aa005718301f1ac56009d6de47b70
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-09-27 07:54:04 -0300

    feat: adicionado tratativa de erros

--- nosso_primeiro_projeto/lib/screens/form_screen.dart
+++ nosso_primeiro_projeto/lib/screens/form_screen.dart
@@ -6,7 +6,7 @@ class FormScreen extends StatefulWidget {
   @override
   State<FormScreen> createState() => _FormScreenState();
 }
-
+//https://cdn.pixabay.com/photo/2024/02/26/19/39/monochrome-image-8598798_640.jpg
 class _FormScreenState extends State<FormScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController difficultyController = TextEditingController();
@@ -23,80 +23,89 @@ class _FormScreenState extends State<FormScreen> {
         ),
       ),
       body: Center(
-        child: Container(
-          height: 580,
-          width: 375,
-          decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 3)),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nome',
-                    fillColor: Colors.white70,
-                    filled: true,
+        child: SingleChildScrollView(
+          child: Container(
+            height: 580,
+            width: 375,
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'dificuldade',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: difficultyController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'dificuldade',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (text){
-                      setState(() {
-
-                      });
-                  },
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'image',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onChanged: (text){
+                        setState(() {
+          
+                        });
+                    },
+                    controller: imageController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'image',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 2, color: Colors.blue),
+                Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                        imageController.text,
+                        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                          return Image.asset('assets/images/nophoto.jpg');
+                        },
+                        fit: BoxFit.cover),
+                  ),
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(imageController.text, fit: BoxFit.cover),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                child: const Text('Adicionar!',  style: TextStyle(color: Colors.white),),
-              )
-            ],
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('Adicionar!',  style: TextStyle(color: Colors.white),),
+                )
+              ],
+            ),
           ),
         ),
       ),
