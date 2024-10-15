26c6690d824619317fd1e839cf4006dc713a9697
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-10-14 22:11:56 -0300

    feat: navegator push e pop

--- nosso_primeiro_projeto/lib/screens/form_screen.dart
+++ nosso_primeiro_projeto/lib/screens/form_screen.dart
@@ -130,6 +130,7 @@ class _FormScreenState extends State<FormScreen> {
                             content: Text('Salvando nova Tarefa'),
                           ),
                         );
+                        Navigator.pop(context);
                       }
                     },
                     style: ElevatedButton.styleFrom(
--- nosso_primeiro_projeto/lib/screens/initial_screen.dart
+++ nosso_primeiro_projeto/lib/screens/initial_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
-
   const InitialScreen({super.key});
 
   @override
@@ -10,12 +10,9 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-
-  bool opacidade = true;
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         leading: Container(),
@@ -24,41 +21,30 @@ class _InitialScreenState extends State<InitialScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: const Duration(microseconds: 800),
-        child: ListView(
-          children: const [
-            Task(
-                "Aprendendo Flutter no cafe da manha comendo sucrilhos",
-                'assets/images/img1.jpg',
-                2),
-            Task(
-                'Andar de Bike',
-                'assets/images/img3.jpeg',
-                3),
-            Task('Meditar',
-                'assets/images/img2.jpeg',
-                5),
-            Task("Aprendendo Flutter",
-                'assets/images/img1.jpg',
-                3),
-            Task('Andar de Bike',
-                'assets/images/img2.jpeg',
-                4),
-            Task('Meditar',
-                  'assets/images/img1.jpg',
-                  1),
-            SizedBox(height: 80,)
-          ],
-        ),
+      body: ListView(
+        children: const [
+          Task("Aprendendo Flutter no cafe da manha comendo sucrilhos",
+              'assets/images/img1.jpg', 2),
+          Task('Andar de Bike', 'assets/images/img3.jpeg', 3),
+          Task('Meditar', 'assets/images/img2.jpeg', 5),
+          Task("Aprendendo Flutter", 'assets/images/img1.jpg', 3),
+          Task('Andar de Bike', 'assets/images/img2.jpeg', 4),
+          Task('Meditar', 'assets/images/img1.jpg', 1),
+          SizedBox(
+            height: 80,
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        setState(() {
-          opacidade = !opacidade;
-        });
-      },
-        child: const Icon(Icons.remove_red_eye),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FormScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
