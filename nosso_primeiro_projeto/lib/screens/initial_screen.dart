5800636b996f93b0c7c5de5fb88339691798751c
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-11-03 12:56:03 -0300

    feat: usando findAll() e connectionState.done

--- nosso_primeiro_projeto/lib/screens/initial_screen.dart
+++ nosso_primeiro_projeto/lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/data/task_dao.dart';
 import 'package:nosso_primeiro_projeto/data/task_inherited.dart';
 import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 
@@ -22,16 +23,38 @@ class _InitialScreenState extends State<InitialScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: ListView(
-        children: TaskInherited.of(context).taskList,
+      body: Padding(
         padding: EdgeInsets.only(top: 8, bottom: 70),
+        child: FutureBuilder<List<Task>>(future: TaskDao().findAll(),builder: (context, snapshot){
+          List<Task>? items = snapshot.data;
+          switch (snapshot.connectionState){
+
+            case ConnectionState.none:
+              // TODO: Handle this case.
+            case ConnectionState.waiting:
+              // TODO: Handle this case.
+            case ConnectionState.active:
+              // TODO: Handle this case.
+            case ConnectionState.done:
+              if(snapshot.hasData && items != null){
+                if(items.isNotEmpty){
+                  return ListView.builder(itemCount: items.length,itemBuilder: (BuildContext context, int index){
+                    final Task tarefa = items[index];
+                    return tarefa;
+                  });
+                }
+              }
+          }
+        } ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (contextNew) => FormScreen(taskContext: context,),
+              builder: (contextNew) => FormScreen(
+                taskContext: context,
+              ),
             ),
           );
         },
