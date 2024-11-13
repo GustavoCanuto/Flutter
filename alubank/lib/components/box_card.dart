1fe82e48df326f65d53cc3a94472e02a826da4ce
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-11-13 20:14:10 -0300

    feat: dark mode e theme para color

--- alubank/lib/components/box_card.dart
+++ alubank/lib/components/box_card.dart
@@ -11,7 +11,7 @@ const BoxCard({ Key? key, required this.boxContent }) : super(key: key);
       decoration: BoxDecoration(
         boxShadow: kElevationToShadow[3],
         borderRadius: BorderRadius.circular(10),
-        
+        color: Theme.of(context).cardColor,
       ),
       padding: const EdgeInsets.all(16),
     );
