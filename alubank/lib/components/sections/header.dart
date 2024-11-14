b878fe53dd3cf8f24e7de8cf9f746f7f7e624be2
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-11-14 20:28:14 -0300

    refactor: colocado cores em variaveis

--- alubank/lib/components/sections/header.dart
+++ alubank/lib/components/sections/header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:alubank/themes/theme_colors.dart';
 
 class Header extends StatelessWidget {
   const Header({Key? key}) : super(key: key);
@@ -10,11 +11,7 @@ class Header extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: <Color>[
-            Color.fromRGBO(103, 99, 234, 1.0),
-            Color.fromRGBO(155, 105, 254, 1.0),
-            Color.fromRGBO(195, 107, 255, 1.0),
-          ]
+          colors: ThemeColors.headerGradient
         ),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
       ),
---
+++ alubank/lib/themes/my_theme.dart
@@ -0,0 +1,10 @@
+import 'package:alubank/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData MyTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark,
+);
+
+ 
