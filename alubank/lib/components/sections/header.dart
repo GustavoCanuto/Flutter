13f4715047cc7f000c0fc0d19188d14c798995c5
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-11-15 21:47:13 -0300

    feat: adcionando thema para texto

--- alubank/lib/components/sections/header.dart
+++ alubank/lib/components/sections/header.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:alubank/themes/theme_colors.dart';
-
 class Header extends StatelessWidget {
-  const Header({Key? key}) : super(key: key);
+   Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -15,7 +14,7 @@ class Header extends StatelessWidget {
         ),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
       ),
-      child: const Padding(
+      child: Padding(
         padding: EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -27,12 +26,14 @@ class Header extends StatelessWidget {
                 Text.rich(
                   TextSpan(
                     text: '\$',
-                    style: TextStyle(fontSize: 16),
+                   // style: TextStyle(fontSize: 16), NÃO PRECISA DESSA LINHA PQ JA TA NO TEMA
                     children: <TextSpan>[
                       TextSpan(
                         text: '1000.00',
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                        style: Theme.of(context).textTheme.bodyLarge,
+                    //    style: TextStyle(
+                    //        fontSize: 28, fontWeight: FontWeight.bold), 
+                    // NÃO PRECISA DESSA LINHA PQ JA TA NO TEMA
                       ),
                     ],
                   ),
--- alubank/lib/themes/my_theme.dart
+++ alubank/lib/themes/my_theme.dart
@@ -5,6 +5,13 @@ ThemeData MyTheme = ThemeData(
   primarySwatch: ThemeColors.primaryColor,
   primaryColor: ThemeColors.primaryColor,
   brightness: Brightness.dark,
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 16,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
 );
-
- 
